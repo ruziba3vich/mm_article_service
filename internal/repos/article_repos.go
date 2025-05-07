b8ff6c10df05df9e843233e0c2be10f7673957c4
ruziba3vich <rruzibayevich@gmail.com>
2025-05-07 11:03:02 +0500

    fix article_service: fix according to new model ArticleEntity

--- internal/repos/article_repos.go
+++ internal/repos/article_repos.go
@@ -7,13 +7,13 @@ import (
 )
 
 type ArticleRepo interface {
-	CreateArticle(context.Context, *article_protos.CreateArticleRequest) (*article_protos.CreateArticleResponse, error)
-	UpdateArticle(context.Context, *article_protos.UpdateArticleRequest) (*article_protos.UpdateArticleResponse, error)
-	RewriteArticle(context.Context, *article_protos.RewriteArticleRequest) (*article_protos.RewriteArticleResponse, error)
+	CreateArticle(context.Context, *article_protos.CreateArticleRequest) (*article_protos.ArticleEntity, error)
+	UpdateArticle(context.Context, *article_protos.UpdateArticleRequest) (*article_protos.ArticleEntity, error)
+	RewriteArticle(context.Context, *article_protos.RewriteArticleRequest) (*article_protos.ArticleEntity, error)
 	DeleteArticle(context.Context, *article_protos.DeleteArticleRequest) (*article_protos.DeleteArticleResponse, error)
 	LikeArticle(context.Context, *article_protos.LikeArticleRequest) (*article_protos.LikeArticleResponse, error)
 	UnlikeArticle(context.Context, *article_protos.UnlikeArticleRequest) (*article_protos.UnlikeArticleResponse, error)
 	GetArticlesByUser(context.Context, *article_protos.GetArticlesByUserRequest) (*article_protos.GetArticlesByUserResponse, error)
 	GetArticles(context.Context, *article_protos.GetArticlesRequest) (*article_protos.GetArticlesResponse, error)
-	GetArticleByID(context.Context, *article_protos.GetArticleByIDRequest) (*article_protos.GetArticleByIDResponse, error)
+	GetArticleByID(context.Context, *article_protos.GetArticleByIDRequest) (*article_protos.ArticleEntity, error)
 }
