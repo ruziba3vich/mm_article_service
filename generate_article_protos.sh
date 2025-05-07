a4663654178338b818eaa8b3b47802f1b3637661
ruziba3vich <rruzibayevich@gmail.com>
2025-05-07 06:26:24 +0500

    feat article_service: implement script to generate protos

---
+++ generate_article_protos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# chmod +x generate_article_protos.sh
+
+set -e
+
+PROTO_DIR="./protos"
+
+OUT_DIR="./genprotos"
+
+mkdir -p $OUT_DIR
+
+protoc -I=$PROTO_DIR \
+  --go_out=$OUT_DIR \
+  --go-grpc_out=$OUT_DIR \
+  $PROTO_DIR/protos/article_protos.proto
+
+echo "protos generated successfully"
