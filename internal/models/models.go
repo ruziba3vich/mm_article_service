a3912e8439b503525225f05a7abb3dd762c4ff5e
ruziba3vich <rruzibayevich@gmail.com>
2025-05-07 15:38:09 +0500

    feat article_service: create pictures entity

--- internal/models/models.go
+++ internal/models/models.go
@@ -7,21 +7,27 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-type Article struct {
-	ID                string    `gorm:"primaryKey;type:uuid;"`
-	UserID            string    `gorm:"type:uuid;not null"`
-	OriginalArticleID string    `gorm:"type:uuid"`
-	Title             string    `gorm:"not null"`
-	Content           string    `gorm:"not null"`
-	CreatedAt         time.Time `gorm:"autoCreateTime"`
-	LikesCount        int       `gorm:"not null;default:0"`
-}
+type (
+	Article struct {
+		ID                string    `gorm:"primaryKey;type:uuid;"`
+		UserID            string    `gorm:"type:uuid;not null"`
+		OriginalArticleID string    `gorm:"type:uuid"`
+		Title             string    `gorm:"not null"`
+		Content           string    `gorm:"not null"`
+		CreatedAt         time.Time `gorm:"autoCreateTime"`
+		LikesCount        int       `gorm:"not null;default:0"`
+	}
 
-type ArticleLike struct {
-	UserID    string    `gorm:"type:uuid;not null;primaryKey"`
-	ArticleID string    `gorm:"type:uuid;not null;primaryKey"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-}
+	ArticleLike struct {
+		UserID    string    `gorm:"type:uuid;not null;primaryKey"`
+		ArticleID string    `gorm:"type:uuid;not null;primaryKey"`
+		CreatedAt time.Time `gorm:"autoCreateTime"`
+	}
+
+	Picture struct {
+		FileName string `gorm:"not null"`
+	}
+)
 
 func (a *Article) ToArticleEntity() *article_protos.ArticleEntity {
 	return &article_protos.ArticleEntity{
