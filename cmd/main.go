7eb572e957e713e642f5021775aff326bf715c8e
ruziba3vich <rruzibayevich@gmail.com>
2025-05-08 01:15:51 +0500

    feat article_service: impement singly entry point

---
+++ cmd/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	_ "github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/ruziba3vich/mm_article_service/genprotos/genprotos/article_protos"
+	"github.com/ruziba3vich/mm_article_service/genprotos/genprotos/user_protos"
+	"github.com/ruziba3vich/mm_article_service/internal/service"
+	"github.com/ruziba3vich/mm_article_service/internal/storage"
+	"github.com/ruziba3vich/mm_article_service/pkg/config"
+	logger "github.com/ruziba3vich/prodonik_lgger"
+	"go.uber.org/fx"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"gorm.io/gorm"
+)
+
+func main() {
+	app := fx.New(
+		fx.Provide(
+			config.LoadConfig,
+			newLogger,
+			newUserServiceClient,
+			storage.NewGORM,
+			storage.NewArticleRepository,
+			storage.NewFileDbStorage,
+			storage.NewMinIOStorage,
+			service.NewArticleService,
+			newGrpcServer,
+		),
+		fx.Invoke(registerHooks),
+	)
+
+	app.Run()
+}
+
+// Create a new gRPC server and register the logging service
+func newGrpcServer(srv *service.ArticleService) *grpc.Server {
+	server := grpc.NewServer()
+	article_protos.RegisterArticleServiceServer(server, srv)
+	return server
+}
+
+// Register application lifecycle hooks
+func registerHooks(
+	lc fx.Lifecycle,
+	db *gorm.DB,
+	grpcServer *grpc.Server,
+	cfg *config.Config,
+) {
+	lc.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			log.Println("Starting logging service...")
+
+			listener, err := net.Listen("tcp", fmt.Sprintf(":%s", cfg.GRPCPort))
+			if err != nil {
+				return fmt.Errorf("failed to listen on port %s: %s", cfg.GRPCPort, err.Error())
+			}
+
+			log.Printf("gRPC server listening on port %s", cfg.GRPCPort)
+
+			go func() {
+				if err := grpcServer.Serve(listener); err != nil {
+					log.Fatalf("Failed to serve gRPC: %v", err)
+				}
+			}()
+
+			log.Println("Logging service started")
+			return nil
+		},
+		OnStop: func(context.Context) error {
+			log.Println("Stopping logging service...")
+
+			grpcServer.GracefulStop()
+			sqlDB, err := db.DB()
+			if err != nil {
+				log.Printf("Error getting raw db connection: %v", err)
+				return err
+			}
+			if err := sqlDB.Close(); err != nil {
+				log.Printf("Error closing database connection: %v", err)
+			}
+
+			log.Println("Logging service stopped")
+			return nil
+		},
+	})
+
+	// Setup signal handling for graceful shutdown
+	go func() {
+		signals := make(chan os.Signal, 1)
+		signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+		<-signals
+
+		log.Println("Received shutdown signal")
+	}()
+}
+
+func newUserServiceClient(cfg *config.Config, logger *logger.Logger) (user_protos.UserServiceClient, error) {
+	conn, err := grpc.NewClient(cfg.UserService, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		logger.Error("Failed to connect to Python Executor Service", map[string]any{"error": err})
+		return nil, err
+	}
+	logger.Info("Connected to gRPC service", map[string]any{"address": cfg.UserService})
+	return user_protos.NewUserServiceClient(conn), nil
+}
+
+func newLogger() (*logger.Logger, error) {
+	return logger.NewLogger("app.log")
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/ruziba3vich/mm_article_service
 go 1.24.2
 
 require (
+	github.com/ClickHouse/clickhouse-go/v2 v2.34.0
 	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
 	github.com/minio/minio-go/v7 v7.0.91
 	github.com/oklog/ulid/v2 v2.1.0
 	github.com/ruziba3vich/prodonik_lgger v1.0.0
+	go.uber.org/fx v1.23.0
 	google.golang.org/grpc v1.72.0
 	google.golang.org/protobuf v1.36.6
 	gorm.io/driver/postgres v1.5.11
@@ -14,25 +17,40 @@ require (
 )
 
 require (
+	github.com/ClickHouse/ch-go v0.65.1 // indirect
+	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-faster/city v1.0.1 // indirect
+	github.com/go-faster/errors v0.7.1 // indirect
 	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
-	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/minio/crc64nvme v1.0.1 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/paulmach/orb v0.11.1 // indirect
+	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/rs/xid v1.6.0 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	go.opentelemetry.io/otel v1.35.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
+	go.uber.org/dig v1.18.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
