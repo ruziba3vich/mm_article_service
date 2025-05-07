b783460f280c668b8b4f5425657f2107e8c31c34
ruziba3vich <rruzibayevich@gmail.com>
2025-05-07 06:50:08 +0500

    feat article_service: create abstract service for article storage

---
+++ internal/repos/article_repos.go
@@ -0,0 +1,19 @@
+package repos
+
+import (
+	"context"
+
+	"github.com/ruziba3vich/mm_article_service/genprotos/genprotos/article_protos"
+)
+
+type ArticleRepo interface {
+	CreateArticle(context.Context, *article_protos.CreateArticleRequest) (*article_protos.CreateArticleResponse, error)
+	UpdateArticle(context.Context, *article_protos.UpdateArticleRequest) (*article_protos.UpdateArticleResponse, error)
+	RewriteArticle(context.Context, *article_protos.RewriteArticleRequest) (*article_protos.RewriteArticleResponse, error)
+	DeleteArticle(context.Context, *article_protos.DeleteArticleRequest) (*article_protos.DeleteArticleResponse, error)
+	LikeArticle(context.Context, *article_protos.LikeArticleRequest) (*article_protos.LikeArticleResponse, error)
+	UnlikeArticle(context.Context, *article_protos.UnlikeArticleRequest) (*article_protos.UnlikeArticleResponse, error)
+	GetArticlesByUser(context.Context, *article_protos.GetArticlesByUserRequest) (*article_protos.GetArticlesByUserResponse, error)
+	GetArticles(context.Context, *article_protos.GetArticlesRequest) (*article_protos.GetArticlesResponse, error)
+	GetArticleByID(context.Context, *article_protos.GetArticleByIDRequest) (*article_protos.GetArticleByIDResponse, error)
+}
