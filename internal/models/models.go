c1c59a940662d181f30023dbd22119a3becf1ca1
ruziba3vich <rruzibayevich@gmail.com>
2025-05-07 10:43:30 +0500

    fix article_service: fix entities

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,19 +1,35 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/ruziba3vich/mm_article_service/genprotos/genprotos/article_protos"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
 
 type Article struct {
-	ID                string    `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	ID                string    `gorm:"primaryKey;type:uuid;"`
 	UserID            string    `gorm:"type:uuid;not null"`
 	OriginalArticleID string    `gorm:"type:uuid"`
 	Title             string    `gorm:"not null"`
 	Content           string    `gorm:"not null"`
 	CreatedAt         time.Time `gorm:"autoCreateTime"`
-	FileURLs          []string  `gorm:"type:text[]"`
+	LikesCount        int       `gorm:"not null;default:0"`
 }
 
 type ArticleLike struct {
 	UserID    string    `gorm:"type:uuid;not null;primaryKey"`
 	ArticleID string    `gorm:"type:uuid;not null;primaryKey"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
+}
+
+func (a *Article) ToArticleEntity() *article_protos.ArticleEntity {
+	return &article_protos.ArticleEntity{
+		Id:        a.ID,
+		UserId:    a.UserID,
+		Title:     a.Title,
+		Content:   a.Content,
+		CreatedAt: timestamppb.New(a.CreatedAt),
+		LikeCount: int32(a.LikesCount),
+	}
 }
